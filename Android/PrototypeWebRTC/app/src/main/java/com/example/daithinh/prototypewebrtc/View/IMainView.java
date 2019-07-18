50d0282877a478d5c3144fe9e1c703f7b0662f49
TMA <tma@TMAs-Mac-mini.local>
2019-07-18 11:29:44 +0700

    a

--- Android/PrototypeWebRTC/app/src/main/java/com/example/daithinh/prototypewebrtc/View/IMainView.java
+++
@@ -1,8 +0,0 @@
-package com.example.daithinh.prototypewebrtc.View;
-
-public interface IMainView {
-
-    void initView();
-    void showToast(String message);
-    void openScreen(int screenType);
-}
